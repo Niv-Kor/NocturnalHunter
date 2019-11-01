8847a5bf7cc9f2fd6f555fde2e051c4378f2645b
Niv Kor <nivkor23@gmail.com>
2019-11-01 19:13:09 +0200

    Initial 2

---
+++ NocturnalHunter/Assets/Camera/CamBaseRelocator.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class CamBaseRelocator : MonoBehaviour
+{
+    [Tooltip("The target that's being followed by the camera.")]
+    [SerializeField] private Transform followTarget;
+
+    private void Update() {
+        transform.position = followTarget.position;
+    }
+}
---
+++ NocturnalHunter/Assets/Camera/CursorViewer.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class CursorViewer : MonoBehaviour
+{
+    [Tooltip("View the cursor regularly during gameplay.")]
+    [SerializeField] public bool showCursor = true;
+
+    void Update() {
+        bool pressEsc = Input.GetKeyDown(KeyCode.Escape);
+        bool clickMouse = Input.GetMouseButton(0);
+
+        //toggle cursor
+        if (pressEsc || (!showCursor && clickMouse)) showCursor = !showCursor;
+
+        Cursor.lockState = showCursor ? CursorLockMode.Locked : CursorLockMode.None;
+        Cursor.visible = !showCursor;
+    }
+}
