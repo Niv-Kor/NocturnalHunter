441154349f26b3f9de7745fec5afbb7eaf66a699
Niv Kor <nivkor23@gmail.com>
2019-11-13 03:02:42 +0200

    Worked on ponds in the savana

---
+++ NocturnalHunter/Assets/UnderwaterReflection.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class UnderwaterReflection : MonoBehaviour
+{
+    [Tooltip("The main camera rig of the game.")]
+    [SerializeField] private GameObject cameraRig;
+
+    [Tooltip("Upper water object.")]
+    [SerializeField] private GameObject upperWaterLevel;
+
+    [Tooltip("Lower water object.")]
+    [SerializeField] private GameObject lowerWaterLevel;
+
+    [Tooltip("The light under the water.")]
+    [SerializeField] private GameObject underwaterShader;
+
+    [Tooltip("The bubbles under the water.")]
+    [SerializeField] private GameObject bubbles;
+
+    private float waterLevel;
+
+    private void Start() {
+        float upperLevel = upperWaterLevel.transform.position.y;
+        float lowerLevel = lowerWaterLevel.transform.position.y;
+        this.waterLevel = (upperLevel + lowerLevel) / 2;
+    }
+
+    private void Update() {
+        bool aboveWater = cameraRig.transform.position.y > waterLevel;
+        upperWaterLevel.SetActive(aboveWater);
+        lowerWaterLevel.SetActive(!aboveWater);
+        underwaterShader.SetActive(!aboveWater);
+        bubbles.SetActive(!aboveWater);
+    }
+}
